2445d6c8d2a9772d3a2d2fae369579031332edb9
Maria Lindling <maria.neith.lindling@protonmail.com>
2025-05-05 13:56:50 +0200

    Added governmenttypes enum

--- PlanetGen/Enums/GovernmentType.cs
+++ PlanetGen/Enums/GovernmentType.cs
@@ -2,7 +2,35 @@ namespace SectorGen2.PlanetGen.Enums ;
 
 public enum GovernmentType
 {
-  None = 0,
-
+  None = 0x00,
   
+  CompanyCorporation = 0x01,
+
+  ParticipatingDemocracy = 0x02,
+
+  SelfPerpetuatingOligarchy = 0x03,
+
+  RepresentativeDemocracy = 0x04,
+
+  FeudalTechnocracy = 0x05,
+
+  CaptiveGovernment = 0x06,
+
+  Balkanization = 0x07,
+
+  CivilServiceBureaucracy = 0x08,
+
+  ImpersonalBureaucracy = 0x09,
+
+  CharismaticDictator = 0x0A,
+
+  NonCharismaticLeader = 0x0B,
+
+  CharismaticOligarchy = 0x0C,
+
+  ReligiousDictatorship = 0x0D,
+
+  ReligiousAutocracy = 0x0E,
+
+  TotalitarianOligarchy = 0x0F,
 }
